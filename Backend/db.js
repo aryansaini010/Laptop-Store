bd93b1b53d7442eca7918e951848394a0cd73bc8
Aryan Saini <sainiaru1111@gmail.com>
2025-09-08 18:18:27 +0530

    Updated MongoDB connection string to new_laptop_store

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/new_laptop_store', {
+    await mongoose.connect("mongodb+srv://ajsainii0001_db_user:s9PneYak6QZy1zDK@laptopstorecluster.ml8ed15.mongodb.net/new_laptop_store?retryWrites=true&w=majority&appName=LaptopStoreCluster", {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
